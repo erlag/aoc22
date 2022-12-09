234251f45811243cfcd1ce50a7e5ce50333d9cfa
Erik Lagercrantz <erik.lagercrantz@klarna.com>
2022-12-09 23:19:14 +0100

    Catch undefined to allow checking partially implemented solutions

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
 module Main (main) where
 import System.Environment ( getArgs )
-import Control.Exception ( catch, IOException )
+import Control.Exception ( evaluate, handle, handleJust, IOException, ErrorCall, Exception )
 import Control.Monad (when)
 import Data.Function ((&))
 import Data.Functor ((<&>))
+import Data.List (isPrefixOf)
 import Util ( (▷), orError, padZip )
 import qualified Day1
 import qualified Day2
@@ -13,10 +14,11 @@ import qualified Day5
 import qualified Day6
 import qualified Day7
 import qualified Day8
+import Data.Maybe (catMaybes)
 
 type Solution = String -> [String]
 
-data CheckOutcome = Missing | New | Correct | Wrong deriving (Eq, Show)
+data Assessment = Missing | New | Correct | Wrong deriving (Eq, Show)
 
 main :: IO ()
 main = do
@@ -45,33 +47,47 @@ solutions =
     , ("day8", Day8.run ▷ map show)
     ]
 
-invoke :: Solution -> IO String -> IO String
-invoke !solution = fmap (solution ▷ unlines)
+invoke :: (a -> [String]) -> IO a -> IO String
+invoke !solution inputAction = do
+    input <- inputAction
+    maybeOutputs <- mapM maybeUndefined $ solution input
+    return $ unlines $ catMaybes maybeOutputs
 
 check :: (String, Solution) -> IO ()
 check (name, solution) = do
     let inputFile =  "./inputs/" ++ name ++ ".txt"
     let outputFile = "./outputs/" ++ name ++ ".txt"
     actual <- inputFile & readFile & invoke solution <&> words
-    expected <- (outputFile & readFile <&> words) `orFallback` []
-    let evaluation = padZip expected actual & map evaluate
-    putStrLn $ showEvaluation name expected actual evaluation
-    let shouldUpdate = New `elem` evaluation && not (Missing `elem` evaluation || Wrong `elem` evaluation)
+    expected <- outputFile & readFile <&> words & ioFallback []
+    let assessment = padZip expected actual & map assess
+    putStrLn $ showAssessment name expected actual assessment
+    let shouldUpdate = New `elem` assessment && not (Missing `elem` assessment || Wrong `elem` assessment)
     when shouldUpdate $ writeFile outputFile (unlines actual)
 
-showEvaluation :: String -> [String] -> [String] -> [CheckOutcome] -> String
-showEvaluation name expected actual evaluation = name ++ " " ++ evaluationSummary ++ " " ++ values ++ comment
+showAssessment :: String -> [String] -> [String] -> [Assessment] -> String
+showAssessment name expected actual assessment = name ++ " " ++ summary ++ " " ++ values ++ comment
     where
-        evaluationSummary = evaluation & map (\case { Missing -> '-'; New -> '+'; Correct -> '='; Wrong -> '!' })
+        summary = assessment & map (\case { Missing -> '-'; New -> '+'; Correct -> '='; Wrong -> '!' })
         values = unwords actual
         comment | hasProblem = " (expected " ++ unwords expected ++ ")"
                 | otherwise = ""
-        hasProblem = Missing `elem` evaluation || Wrong `elem` evaluation
+        hasProblem = Missing `elem` assessment || Wrong `elem` assessment
 
-orFallback :: IO a -> a -> IO a
-task `orFallback` value = catch task (\case (err::IOException) -> return value)
 
-evaluate :: Eq a => (Maybe a, Maybe a) -> CheckOutcome
-evaluate (Just _, Nothing) = Missing
-evaluate (Nothing, Just _) = New
-evaluate (Just a, Just b) | a == b = Correct | otherwise = Wrong
+assess :: Eq a => (Maybe a, Maybe a) -> Assessment
+assess (Just _, Nothing) = Missing
+assess (Nothing, Just _) = New
+assess (Just a, Just b) | a == b = Correct | otherwise = Wrong
+
+
+ioFallback :: a -> IO a -> IO a
+ioFallback value = handle (\case (err::IOException) -> return value)
+
+fallback :: Exception e => (e -> Bool) -> a -> IO a -> IO a
+fallback p x = handleJust (\e -> if p e then Just x else Nothing) return
+
+isUndefinedError :: ErrorCall -> Bool
+isUndefinedError (e::ErrorCall) = "Prelude.undefined" `isPrefixOf` show e
+
+maybeUndefined :: a -> IO (Maybe a)
+maybeUndefined x = fallback isUndefinedError Nothing (Just <$> evaluate x)
